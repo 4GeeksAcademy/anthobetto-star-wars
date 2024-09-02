406bd29c532afd753c155eda4218570e2e52582a
Anthony Flores <64795591+Anthobetto@users.noreply.github.com>
2024-09-02 19:27:45 +0000

    [initial] fix: style code according  best practices

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
-
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+// Custom Components
+import ScrollToTop from "./component/ScrollToTop";
+import { BackendURL } from "./component/BackendURL";
+import { Navbar } from "./component/Navbar";
+import { Footer } from "./component/Footer";
+//Custom Pages / View
+import { Home } from "./pages/Home";
+import { Demo } from "./pages/Demo";
+import { Single } from "./pages/Single";
 
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
-
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
         <div>
