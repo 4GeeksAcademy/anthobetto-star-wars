8ca9659877d53b81046f3d6fd5fb5bcea83d661e
Anthony Flores <64795591+Anthobetto@users.noreply.github.com>
2024-09-02 19:40:05 +0000

    [initial] fix: style coding best practices

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import injectContext from "./store/appContext";
 // Custom Components
-import ScrollToTop from "./component/ScrollToTop";
-import { BackendURL } from "./component/BackendURL";
-import { Navbar } from "./component/Navbar";
-import { Footer } from "./component/Footer";
+import ScrollToTop from "./component/ScrollToTop.jsx";
+import { BackendURL } from "./component/BackendURL.jsx";
+import { Navbar } from "./component/Navbar.jsx";
+import { Footer } from "./component/Footer.jsx";
 //Custom Pages / View
-import { Home } from "./pages/Home";
-import { Demo } from "./pages/Demo";
-import { Single } from "./pages/Single";
+import { Home } from "./pages/Home.jsx";
+import { Demo } from "./pages/Demo.jsx";
+import { Single } from "./pages/Single.jsx";
 
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
-    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
     return (
         <div>
             <BrowserRouter basename={basename}>
@@ -27,7 +27,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<h1>Not found!</h1>} path="*"/>
                     </Routes>
                     <Footer />
                 </ScrollToTop>
