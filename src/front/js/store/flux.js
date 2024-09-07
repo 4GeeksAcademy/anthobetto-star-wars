ba6f2815bfc17c7fa7e7a778d87e50d09fdd8b2e
Anthony Flores <64795591+Anthobetto@users.noreply.github.com>
2024-09-07 18:51:52 +0000

    feat: Edit component, Add & Edit: new button for cancel, Flux: action editContact

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,14 +1,13 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			host: 'https://playground.4geeks.com/contact',
-			message: null,
-			user: 'anthobetto',
-			contacts: [],
-			currentContacts: {},
-			agendauid: null,
-		},
-		actions: {
+    return {
+        store: {
+            host: 'https://playground.4geeks.com/contact',
+            message: null,
+            user: 'anthobetto',
+            contacts: [],
+            currentContacts: {},
+        },
+        actions: {
             getMessage: async () => {
                 const response = await fetch(process.env.BACKEND_URL + "/api/hello")
                 if (!response.ok) {
@@ -20,61 +19,73 @@ const getState = ({ getStore, getActions, setStore }) => {
                 // don't forget to return something, that is how the async resolves
                 return data;
             },
-			createUser: async () => {
-				const uri = `${getStore().host}/agendas/${getStore().user}`;
-				const options = {
-					method: 'POST'
-				}
-				const response = await fetch(uri, options)
-				if (!response.ok) {
-					return
-				}
-				getContacts()
-		
-			},
-
-			getContacts: async () => {
-				const uri = `${getStore().host}/agendas/${getStore().user}/contacts`;
-				const options = {
-					method: 'GET'
-				};
-				const response = await fetch(uri, options);
-				console.log(response);
-				if (!response.ok) {
-					return createUser()
-				}
-				const data = await response.json();
-				setStore({contacts: data.contacts});
-			},
-
-			addContact: async (dataToSend) => {
-				const uri = `${getStore().host}/agendas/${getStore().user}/contacts`;
-				const options = {
-					method: 'POST',
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify(dataToSend)
-				};
-				const response = await fetch(uri, options);
-				if (!response.ok) {
-					return;
-				}
-				getActions().getContacts()
-			},
-			deleteContact: async (designateNumber) => {
-				const uri = `${getStore().host}/agendas/${getStore().user}/contacts/${designateNumber}`;
-				const options = {
-					method: 'DELETE'
-				};
-				const response = await fetch(uri, options);
-				if (!response.ok) {
-					return;
-				}
-				getActions().getContacts();
-			},
-		}
-	};
+            setCurrentContacts: (contacts) => { setStore({currentContacts: contacts}) },
+            createUser: async () => {
+                const uri = `${getStore().host}/agendas/${getStore().user}`;
+                const options = {
+                    method: 'POST'
+                }
+                const response = await fetch(uri, options)
+                if (!response.ok) {
+                    return
+                }
+                getContacts()
+            },
+            getContacts: async () => {
+                const uri = `${getStore().host}/agendas/${getStore().user}/contacts`;
+                const options = {
+                    method: 'GET'
+                };
+                const response = await fetch(uri, options);
+                console.log(response);
+                if (!response.ok) {
+                    return createUser()
+                }
+                const data = await response.json();
+                setStore({contacts: data.contacts});
+            },
+            editContact: async (id, dataToSend) => {
+                const uri = `${getStore().host}/agendas/${getStore().user}/contacts/${id}`
+                const options = {
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(dataToSend)
+                }
+                const response = await fetch(uri, options)
+                if (!response.ok) {
+                    return
+                }
+                getActions().getContacts();
+            },
+            addContact: async (dataToSend) => {
+                const uri = `${getStore().host}/agendas/${getStore().user}/contacts`;
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(dataToSend)
+                };
+                const response = await fetch(uri, options);
+                if (!response.ok) {
+                    return;
+                }
+                getActions().getContacts()
+            },
+            deleteContact: async (designateNumber) => {
+                const uri = `${getStore().host}/agendas/${getStore().user}/contacts/${designateNumber}`;
+                const options = {
+                    method: 'DELETE'
+                };
+                const response = await fetch(uri, options);
+                if (!response.ok) {
+                    return;
+                }
+                getActions().getContacts();
+            },
+        }
+    };
 };
-
 export default getState;
