ce075b722c6f647ffcfddfc8668af2a31e01e53e
Anthony Flores <64795591+Anthobetto@users.noreply.github.com>
2024-09-30 19:39:29 +0000

    update: routes and models

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users
+from .models import db, Users, Posts, Comments, Media, Characters, FavoritesCharacters, Planets, FavoritesPlanets, Starships, FavoritesStarships, Followers
 
 
 def setup_admin(app):
@@ -9,3 +9,13 @@ def setup_admin(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
     admin.add_view(ModelView(Users, db.session))  # You can duplicate that line to add mew models
+    admin.add_view(ModelView(Posts, db.session)) 
+    admin.add_view(ModelView(Comments, db.session)) 
+    admin.add_view(ModelView(Media, db.session))
+    admin.add_view(ModelView(Characters, db.session))
+    admin.add_view(ModelView(FavoritesCharacters, db.session))
+    admin.add_view(ModelView(Planets, db.session))
+    admin.add_view(ModelView(FavoritesPlanets, db.session))
+    admin.add_view(ModelView(Starships, db.session))
+    admin.add_view(ModelView(FavoritesStarships, db.session))
+    admin.add_view(ModelView(Followers, db.session))
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from datetime import datetime
+from api.models import db, Users, Posts, Characters, Planets
+import requests
 
 
 api = Blueprint('api', __name__)
@@ -14,5 +16,134 @@ CORS(api) # Allow CORS requests to this API
 @api.route('/hello', methods=['GET'])
 def handle_hello():
     response_body = {}    
-    response_body["message"] = """Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"""
+    response_body["message"] = "Hello! I'm a message that came from the backend"
+    return response_body, 200
+
+
+@api.route('/users')
+def users():
+    response_body = {}
+    rows = db.session.execute(db.select(Users)).scalars()
+    result = [row.serialize() for row in rows]
+    response_body['message'] = 'Listado de Usuarios y sus publicaciones(GET)'
+    response_body['results'] = result
+    return response_body, 200
+
+
+# Endpoint de Post (CRUD)
+@api.route('/posts', methods=['GET', 'POST'])
+def posts():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Posts)).scalars()
+        # option2 
+        # print(rows)
+        # result = []
+        # for row in rows:
+        #    result.append(row.serialize())
+        # option 1 - list comprenhesion
+        # var = [objetivo for iterador in list]
+        result = [row.serialize() for row in rows]
+        response_body['message'] = 'Listado de todas las Publicaciones (GET)'
+        response_body['results'] = result
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        # validar si estoy recibiendo todas las claves (campos)
+        row = Posts(title = data.get('title'),
+                    description = data.get('description'),
+                    body = data.get('body'),
+                    date = datetime.now(),
+                    image_url = data.get('image_url'),
+                    user_id = data.get('user_id'),)
+        db.session.add(row)
+        db.session.commit()
+        response_body['message'] = 'Creando una Publicación (POST)'
+        response_body['results'] = row.serialize()
+        return response_body, 200
+
+
+@api.route('/posts/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def post(id):
+    response_body =  {}
+    row = db.session.execute(db.select(Posts).where(Posts.id == id)).scalar()
+    if not row:
+        response_body['message'] = f'Method (GET): La publicación {id} no existe'
+        response_body['result'] = {}
+        return response_body, 404
+    # print(row.serialize())
+    if request.method == 'GET':
+        response_body['message'] = f'Method (GET): Obtenemos datos a través del {id}'
+        response_body['result'] = row.serialize()
+        return response_body, 200
+    if request.method == 'PUT':
+        data = request.json
+        print(data)
+        # Valida que reciba todas las claves en el body( json)
+        row.title = data.get('title')
+        row.description = data.get('description')
+        row.body = data.get('body')
+        row.date = datetime.now()
+        row.image_url = data.get('image_url')
+        row.user_id = data.get('user_id')
+        db.session.commit()
+        response_body['message'] = f'Method (PUT): Actualizamos datos a través del {id}'
+        response_body['result'] = row.serialize()
+        return response_body, 200
+    if request.method == 'DELETE':
+        db.session.delete(row)
+        db.session.commit()
+        response_body['message'] = f'Method (DELETE): Borramos datos a través del {id}'
+        response_body['result'] = {}
+        return response_body, 200
+
+
+@api.route('/characters', methods=['GET'])
+def characters():
+    response_body = {}
+    url = "https://swapi.dev/api/people"
+    response = requests.get(url)
+    print(response) 
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        for row in data['results']:
+            Character = Characters(
+                name=row.get("name"),
+                height=row.get("height"),
+                mass=row.get("mass"),
+                hair_color=row.get("hair_color"),
+                skin_color=row.get("skin_color"),
+                eye_color=row.get("eye_color"),
+                birth_year=row.get("birth_year")
+            )
+            db.session.add(Character)       
+        db.session.commit()
+        response_body['results'] = data
+    return response_body, 200
+
+@api.route('/planets', methods=['GET'])
+def planets():
+    response_body = {}
+    url = "https://swapi.dev/api/planets"
+    response = requests.get(url)
+    print(response) 
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        for row in data['results']:
+            Planet = Planets(
+                name=row.get("name"),
+                diameter=row.get("diameter"),
+                rotation_period=row.get("rotation_period"),
+                orbital_period=row.get("orbital_period"),
+                gravity=row.get("gravity"),
+                population=row.get("population"),
+                climate=row.get("climate"),
+                terrain=row.get("terrain"),
+                surface_water=row.get("surface_water"),
+            )
+            db.session.add(Planet)       
+        db.session.commit()
+        response_body['results'] = data
     return response_body, 200
